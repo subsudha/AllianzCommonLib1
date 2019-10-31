a6e5d5fc278cdf703ad933a35acc9ea5a31a9e95
Subin Sudhakaran (c) <subins@vmware.com>
2019-11-01 01:09:25 +0530

    Test Jenkins Deploy1

--- src/main/resources/com/allianz/common/sample.js
+++ src/main/resources/com/allianz/common/sample.js
@@ -1,6 +1,6 @@
 /**
  * Write a brief description of the purpose of the action.
- * Test Jenkins
+ * Test Jenkins deploy1
  *
  * @param {number} x - describe each parameter as in JSDoc format.
  * @param {number} y - you can use different vRO types.
